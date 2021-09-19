1f9cf10180cb323851d3d33df0fb104ba1474feb
Alexcei88 <lex9.darovskoi@gmail.com>
2021-09-19 21:38:29 +0300

    HashTable implementation

---
+++ HashTable/ConsoleTester.UnitTests/HashTableUnitTests.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleTester.UnitTests
+{
+    [TestFixture]
+    public class HashTableUnitTests
+    {
+        [Test]
+        public void ContainsKey()
+        {
+            var hashtable = new HashTable<string, string>(4);
+            FillTable(hashtable);
+            Assert.AreEqual(22, hashtable.Size());
+            
+            Assert.IsTrue(hashtable.ContainsKey("Hendrix"));
+            Assert.IsTrue(hashtable.ContainsKey("Lawson"));
+            Assert.IsTrue(hashtable.ContainsKey("Mekhi"));
+            Assert.IsTrue(hashtable.ContainsKey("Gerald"));
+            Assert.IsFalse(hashtable.ContainsKey("Hendrix1"));
+            Assert.IsFalse(hashtable.ContainsKey("Hendrix2"));
+        }
+        
+        [Test]
+        public void Contains()
+        {
+            var hashtable = new HashTable<string, string>(4);
+            FillTable(hashtable);
+            Assert.AreEqual(22, hashtable.Size());
+            
+            Assert.IsTrue(hashtable.Contains("Heath"));
+            Assert.IsTrue(hashtable.Contains("Cristiano"));
+            Assert.IsTrue(hashtable.Contains("Wade"));
+            Assert.IsFalse(hashtable.Contains("Heath1"));
+            Assert.IsFalse(hashtable.Contains("Heath2"));
+        }
+        
+        [Test]
+        public void Get()
+        {
+            var hashtable = new HashTable<string, string>(8);
+            FillTable(hashtable);
+            
+            Assert.AreEqual("Cyrus", hashtable.Get("Hendrix"));
+            Assert.AreEqual("Dillon", hashtable.Get("Lawson"));
+            Assert.AreEqual("Makai", hashtable.Get("Mekhi"));
+            Assert.AreEqual("Allan", hashtable.Get("Gerald"));
+            Assert.AreNotEqual("Cyrus1", hashtable.Get("Hendrix"));
+            Assert.AreNotEqual("Cyrus2", hashtable.Get("Hendrix2"));
+        }
+        
+        [Test]
+        public void Remove()
+        {
+            var hashtable = new HashTable<string, string>(8);
+            FillTable(hashtable);
+
+            Assert.AreEqual(22, hashtable.Size());
+            
+            Assert.IsTrue(hashtable.ContainsKey("Hendrix"));
+            Assert.IsTrue(hashtable.ContainsKey("Lawson"));
+            
+            hashtable.Remove("Hendrix");
+            hashtable.Remove("Lawson");
+            Assert.AreEqual(20, hashtable.Size());
+            Assert.IsFalse(hashtable.ContainsKey("Hendrix"));
+            Assert.IsFalse(hashtable.ContainsKey("Lawson"));
+            
+            hashtable.Remove("Hendrix");
+            Assert.AreEqual(20, hashtable.Size());
+        }
+        
+        [Test]
+        public void Clear()
+        {
+            var hashtable = new HashTable<string, string>(8);
+            FillTable(hashtable);
+
+            Assert.AreEqual(22, hashtable.Size());
+            
+            Assert.IsTrue(hashtable.ContainsKey("Hendrix"));
+            Assert.IsTrue(hashtable.ContainsKey("Lawson"));
+            
+            hashtable.Clear();
+            Assert.AreEqual(0, hashtable.Size());
+            Assert.IsFalse(hashtable.ContainsKey("Hendrix"));
+            Assert.IsFalse(hashtable.ContainsKey("Lawson"));
+        }
+        
+        private void FillTable(HashTable<string, string> hashtable)
+        {
+            hashtable.Put("Brock", "Jamison");
+            hashtable.Put("John", "Cyrus");
+            hashtable.Put("Augustus", "Royal");
+            hashtable.Put("Hendrix", "Cyrus");
+            hashtable.Put("Lawson", "Dillon");
+            hashtable.Put("Moses", "Wade");
+            hashtable.Put("Aarav", "Asa");
+            hashtable.Put("Marvin", "Ahmad");
+            hashtable.Put("Ezequiel", "Emmitt");
+            hashtable.Put("Aldo", "Abdullah");
+            hashtable.Put("Samson", "Jaxen");
+            hashtable.Put("Mekhi", "Makai");
+            hashtable.Put("Sage", "Allan");
+            hashtable.Put("Ezequiel", "Rohan");
+            hashtable.Put("Zain", "Valentino");
+            hashtable.Put("Layne", "Makai");
+            hashtable.Put("Sonny", "Allan");
+            hashtable.Put("Samir", "Rohan");
+            hashtable.Put("Wayne", "Valentino");
+            hashtable.Put("Gerald", "Allan");
+            hashtable.Put("Leandro", "Heath");
+            hashtable.Put("Fisher", "Cristiano");
+        }
+    }
+}
---
+++ HashTable/HashTable/HashTable.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleTester
+{
+    public class HashTable<K, V>
+    {
+        private sealed class Bucket
+        {
+            public readonly K Key;
+            public readonly V Value;
+
+            public Bucket(K key, V value)
+            {
+                Key = key;
+                Value = value;
+            }
+
+            public override int GetHashCode()
+            {
+                return Key.GetHashCode() + Value.GetHashCode();
+            }
+        }
+        
+        private const uint Randomhashfactor = 43123578;
+        private double _fillFactor = 0.75;
+        private LinkedList<Bucket>[] _table;
+        private int _resizeThreshold;
+        private static int DEFAULT_CAPACITY = 16;
+        
+        private int _size;
+
+        public HashTable()
+            : this(DEFAULT_CAPACITY)
+        { }
+        
+        public HashTable(int capacity)
+        {
+            _table = new LinkedList<Bucket>[capacity];
+            _resizeThreshold = (int) (_table.Length * _fillFactor);
+        }
+
+        public int Size() => _size;
+        
+        public void Put(K key, V obj)
+        {
+            if (key == null)
+                throw new ArgumentException($"{nameof(key)} is null");
+            
+            var newBucket = new Bucket(key, obj);
+            if (++_size > _resizeThreshold)
+                Expand();
+            
+            uint idx = CalculateHash(key);
+            
+            LinkedList<Bucket> curBucket = _table[idx];
+            if (curBucket == null)
+            {
+                _table[idx] = new LinkedList<Bucket>();
+                _table[idx].AddFirst(newBucket);
+            }
+            else
+            {
+                curBucket.AddFirst(newBucket);
+            }
+        }
+
+        public bool Contains(V obj)
+        {
+            foreach (var lst in _table)
+            {
+                if (lst != null)
+                {
+                    foreach (var item in lst)
+                        if (item.Value.Equals(obj))
+                            return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool ContainsKey(K key)
+        {
+            uint idx = CalculateHash(key);
+            LinkedList<Bucket> bucket = _table[idx];
+            if (bucket == null)
+                return false;
+
+            foreach (var item in bucket)
+            {
+                if (item.Key.Equals(key))
+                    return true;
+            }
+            return false;
+        }
+
+        public V Get(K key)
+        {
+            uint idx = CalculateHash(key);
+            LinkedList<Bucket> bucket = _table[idx];
+            if (bucket == null)
+                return default;
+
+            foreach (var item in bucket)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
+            }
+            return default;
+        }
+
+        public V Remove(K key)
+        {
+            uint idx = CalculateHash(key);
+            LinkedList<Bucket> bucket = _table[idx];
+            if (bucket == null)
+                return default;
+            
+            foreach (var item in bucket)
+            {
+                if (item.Key.Equals(key))
+                {
+                    bucket.Remove(item);
+                    --_size;
+                    return item.Value;
+                }
+            }
+
+            return default;
+        }
+        
+        public void Clear()
+        {
+            _table = new LinkedList<Bucket>[DEFAULT_CAPACITY];
+            _size = 0;
+            _resizeThreshold = (int) (DEFAULT_CAPACITY * _fillFactor);
+        }
+
+        private void Expand()
+        {
+            Resize(_table.Length * 2 + 1);
+        }
+        
+        private uint CalculateHash(K key)
+        {
+            return (uint) (((uint)key.GetHashCode() * Randomhashfactor) % _table.Length);
+        }
+
+        private void Resize(int size)
+        {
+            LinkedList<Bucket>[] oldTable = _table;
+            _resizeThreshold = (int) (size * _fillFactor);
+            _table = new LinkedList<Bucket>[size];
+
+            for (int i = oldTable.Length - 1; i >=0; --i)
+            {
+                LinkedList<Bucket> oldBucket = oldTable[i];
+                if (oldBucket != null)
+                {
+                    foreach (var item in oldBucket)
+                    {
+                        uint idx = CalculateHash(item.Key);
+                        var newBucket = _table[idx];
+                        if (newBucket == null)
+                            _table[idx] = new LinkedList<Bucket>();
+
+                        _table[idx].AddLast(item);
+                    }
+                }
+            }
+        }
+    }
+}
